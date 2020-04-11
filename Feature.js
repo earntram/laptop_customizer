5ac7329d575def1bfefc51dda6975c29b30e73fd
earntram <53387455+earntram@users.noreply.github.com>
2020-04-11 13:29:18 -0700

    Update and rename Return.js to Feature.js

--- Feature.js
+++ Feature.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import slugify from 'slugify';
 import './App.css';
 
+class Feature extends Component {
+  render() {
 return (
         <fieldset className="feature" key={featureHash}>
           <legend className="feature__name">
@@ -11,3 +13,7 @@ return (
         </fieldset>
       );
     });
+ }
+}
+
+export default Feature;
