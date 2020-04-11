272daebfa1d07b0747d9a647e59efbb506a5fd94
earntram <53387455+earntram@users.noreply.github.com>
2020-04-11 13:31:57 -0700

    Update Summary.js

--- Summary.js
+++ Summary.js
@@ -1,7 +1,13 @@
+import React, { Component } from 'react';
+import slugify from 'slugify';
+import './App.css';
+
 const summary = Object.keys(this.state.selected).map((feature, idx) => {
       const featureHash = feature + '-' + idx;
       const selectedOption = this.state.selected[feature];
 
+class Summary extends Component {
+  render() {
       return (
         <div className="summary__option" key={featureHash}>
           <div className="summary__option__label">{feature} </div>
