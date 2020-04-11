514bee5e66ed2902f85aff952f59eeadcf5972cf
earntram <53387455+earntram@users.noreply.github.com>
2020-04-11 13:25:17 -0700

    Create Return.js

---
+++ Return.js
@@ -0,0 +1,9 @@
+return (
+        <fieldset className="feature" key={featureHash}>
+          <legend className="feature__name">
+            <h3>{feature}</h3>
+          </legend>
+          {options}
+        </fieldset>
+      );
+    });
